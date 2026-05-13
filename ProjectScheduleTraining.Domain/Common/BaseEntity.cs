0ed4edbcc4b27ed0a42944d706766b00f0199c79
Geovane Silva Costa <9904553@prestadorserv.com.br>
2026-05-13 12:57:30 -0300

    feat: Common e Exceptions

---
+++ ProjectScheduleTraining.Domain/Common/BaseEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace ProjectScheduleTraining.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+        public bool IsDeleted { get; private set; }
+
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        protected void SetUpdated() => UpdatedAt = DateTime.UtcNow;
+
+        public void SoftDelete()
+        {
+            IsDeleted = true;
+            SetUpdated();
+        }
+    }
+}
---
+++ ProjectScheduleTraining.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectScheduleTraining.Domain.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public string Code { get; }
+
+        public DomainException(string message, string code = "DOMAIN_ERROR")
+            : base(message)
+        {
+            Code = code;
+        }
+    }
+}
